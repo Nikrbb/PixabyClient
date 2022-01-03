c93d22900409d235425c3d58750e895fcb00d720
Nikrbb <81448903+Nikrbb@users.noreply.github.com>
2022-01-03 21:41:25 +0200

    vuex

---
+++ src/components/CatsList.vue
@@ -0,0 +1,41 @@
+<template>
+<div class="container">
+  <div class="card" v-for="card in cards" :key="card.id">
+    <a v-bind:href="card.pageURL" target="_blank" class="card__image">
+      <img v-bind:src="card.largeImageURL" alt="random-picture">
+    </a>
+    <div class="card__likes">{{ card.likes + ` likes`}}</div>
+    <div class="card__comments">{{ card.comments + ` comments`}}</div>
+    <div class="card__tags">
+        <span class="card__tags-item">{{ card.tags }}</span>
+        <span class="card__tags-item">{{ card.tags }}</span>
+        <span class="card__tags-item">{{ card.tags }}</span>
+        <span class="card__tags-item">{{ card.tags }}</span>
+    </div>
+  </div>
+</div>
+</template>
+
+
+<script>
+
+export default {
+  name: 'CatsList',
+  computed: {
+    cards() {
+      return this.$store.getters.getJson
+    },
+    tagsArray() {
+        return this.cards
+    }
+  },
+  mounted() {
+      setTimeout(() => {
+        console.log(this.cards[0]); 
+        console.log(this.tagsArray); 
+      },1000)
+  }
+}
+</script>
+
+
--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,42 @@
 import { createStore } from 'vuex'
+import axios from 'axios';
+
 
 export default createStore({
   state: {
+    catsJson: null,
   },
+
+  getters: {
+    getJson: state => {
+      return state.catsJson;
+    }
+  },
+
   mutations: {
+    SET_JSON(state, payload) {
+      state.catsJson = payload;
+    }
+
   },
   actions: {
+    async loadJson({ commit }) {
+
+      let json;
+      let errors;
+
+      await axios
+      .get(`https://pixabay.com/api/?key=25040037-a12ad33d617bc126ab0283a57&q=cats&image_type=all&per_page=5`)
+      .then(response => {
+        json = response.data.hits;
+        commit(`SET_JSON`, json)
+      })
+      .catch(e => {
+        errors.push(e);
+      })
+      
+    },
+    
   },
   modules: {
   }
